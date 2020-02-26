71508b1f047408d20bfc07f199028bf1f27ad85d
zwiadowca987 <zwiadowca.dyktator@gmail.com>
2020-02-26 23:31:29 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let i = 0;
+	const myStyle = {
+		fontsize: 100,
+		color: '#FF0000'
+	};
+
+	return (
+		<div className="App">
+			Hello World!
+			<br/>
+
+			{i === 1 ? 'True!' : 'False'}
+			<br/>
+
+			<h1 style={myStyle}>Header</h1>
+			<br/>
+
+			{/*Komentarz wieloliniowy*/}
+
+			<Header/>
+			<Content/>
+		</div>
+	);
 }
 
 export default App;
