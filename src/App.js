f5f3dab22a1370578e114c80db88fdb7faa17f73
zwiadowca987 <zwiadowca.dyktator@gmail.com>
2020-02-27 23:16:23 +0100

    Publishing #1

--- src/App.js
+++ src/App.js
@@ -27,4 +27,24 @@ function App() {
 	);
 }
 
+class Header extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Header</h1>
+			</div>
+		);
+	}
+}
+
+class Content extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Content</h1>
+			</div>
+		);
+	}
+}
+
 export default App;
