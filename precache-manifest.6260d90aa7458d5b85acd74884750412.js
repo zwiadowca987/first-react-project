9a1e470886f21f57eda020589ad074b10f53f870
zwiadowca987 <zwiadowca.dyktator@gmail.com>
2020-02-27 23:14:50 +0100

    Updates

---
+++ precache-manifest.6260d90aa7458d5b85acd74884750412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e60153a01df0c26bdc2d23260861b01",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "ea97c88b249676503a80",
+    "url": "/first-react-project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e17ad321dbbc8c72738a",
+    "url": "/first-react-project/static/js/2.ca4c73d4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/first-react-project/static/js/2.ca4c73d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea97c88b249676503a80",
+    "url": "/first-react-project/static/js/main.6e6585a7.chunk.js"
+  },
+  {
+    "revision": "40259924f8ede4a7559f",
+    "url": "/first-react-project/static/js/runtime-main.d64e254e.js"
+  }
+]);
